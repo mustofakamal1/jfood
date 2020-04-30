eaad2475e0cb4ea9f34a3a9d7753d151d6df05ae
mustofakamal1 <mustofa.alhaddad@gmail.com>
2020-04-30 16:29:08 +0700

    CS Modul 9

--- src/main/java/mustofakamal/jfood/JFood.java
+++ src/main/java/mustofakamal/jfood/JFood.java
@@ -6,27 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JFood {
 
     public static void main(String[] args) {
-        Location loc1 = new Location("Jakarta Timur", "Jakarta", "Home");
-        Location loc2 = new Location("Depok", "Jawa Barat", "UI");
-        Location loc3 = new Location("Bekasi", "Jawa Barat", "Jauh");
+        SpringApplication.run(JFood.class, args);
         Seller seller1 = new Seller(DatabaseSeller.getLastId() + 1, "Rafid", "rafid@gmail.com",
-                "0812345678", loc3);
+                "0812345678", new Location("Bekasi", "Jawa Barat", "Jauh"));
         DatabaseSeller.addSeller(seller1);
-        Seller seller2 = new Seller(DatabaseSeller.getLastId() + 1, "Mulya", "mulya@gmail.com",
-                "0856781234", loc2);
-        DatabaseSeller.addSeller(seller2);
-        Seller seller3 = new Seller(DatabaseSeller.getLastId() + 1, "Ilham", "ilham@gmail.com",
-                "0812783456", loc1);
-        DatabaseSeller.addSeller(seller3);
         Food food1 = new Food(DatabaseFood.getLastId() + 1, "food1", seller1, 10000, FoodCategory.Bakery);
         DatabaseFood.addFood(food1);
-        Food food2 = new Food(DatabaseFood.getLastId() + 1, "food2", seller2, 20000, FoodCategory.Coffee);
+        Food food2 = new Food(DatabaseFood.getLastId() + 1, "food2", seller1, 20000, FoodCategory.Coffee);
         DatabaseFood.addFood(food2);
-        Food food3 = new Food(DatabaseFood.getLastId() + 1, "food3", seller3, 80000, FoodCategory.Japanese);
-        DatabaseFood.addFood(food3);
-        Food food4 = new Food(DatabaseFood.getLastId() + 1, "food4", seller1, 15000, FoodCategory.Bakery);
-        DatabaseFood.addFood(food4);
-        SpringApplication.run(JFood.class, args);
     }
 }
 
